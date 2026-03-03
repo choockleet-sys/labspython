776b7e936f8110a4da4da1c07db5e572ca5fbd97
ch0k <choockleet@gmail.com>
2026-03-03 12:09:51 +0500

    Add files via upload

--- labs3python/labs3python1.py
+++ labs3python/labs3python1.py
@@ -4,11 +4,25 @@ def rec(n, s):
     def b(d):
         if d == 1:
             return [f"level {n}"] * s
-        r = []
-        for i in range(s):
-            r.append(b(d-1))
-        return r
-    return b(n)
+        return [b(d - 1) for _ in range(s)]
+    def format_ndim(arr, level=0):
+        indent = '    ' * level
+        if isinstance(arr, list):
+            if not arr:
+                return indent + '[]'
+            if all(not isinstance(e, list) for e in arr):
+                return indent + '[' + ', '.join(repr(e) for e in arr) + ']'
+            else:
+                lines = [indent + '[']
+                for i, e in enumerate(arr):
+                    lines.append(format_ndim(e, level + 1))
+                    if i < len(arr) - 1:
+                        lines[-1] += ','
+                lines.append(indent + ']')
+                return '\n'.join(lines)
+        else:
+            return indent + repr(arr)
+    print(format_ndim(b(n)))
 
 
 
@@ -17,12 +31,26 @@ def mas(n, s):
         return []
     a = [f"level {n}"] * s
     for i in range(2, n + 1):
-        r = []
-        for l in range(s):
-            r.append(copy.deepcopy(a))
-        a = r
-    return a 
-
+        a = [copy.deepcopy(a) for _ in range(s)]
+    def format_ndim(arr, level=0):
+        indent = '    ' * level
+        if isinstance(arr, list):
+            if not arr:
+                return indent + '[]'
+            if all(not isinstance(e, list) for e in arr):
+                return indent + '[' + ', '.join(repr(e) for e in arr) + ']'
+            else:
+                lines = [indent + '[']
+                for i, e in enumerate(arr):
+                    lines.append(format_ndim(e, level + 1))
+                    if i < len(arr) - 1:
+                        lines[-1] += ','
+                lines.append(indent + ']')
+                return '\n'.join(lines)
+        else:
+            return indent + repr(arr)
+    print(format_ndim(a)) 
+    
 def format_ndim(arr, level=0):
     indent = '    ' * level
     if isinstance(arr, list):
@@ -42,8 +70,8 @@ def format_ndim(arr, level=0):
         return indent + repr(arr)
 
 print("Рекурсивный")
-print(format_ndim(rec(3, 2)))
+rec(4, 2)
 
 
 print("Нерекурсивный")
-print(format_ndim(mas(3, 2)))
+mas(4, 2)
