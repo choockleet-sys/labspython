fa4dffe4ab1020c461a4a26d6adb1f40053e372c
ch0k <choockleet@gmail.com>
2026-03-03 10:52:30 +0500

    Add files via upload

--- labs3python/labs3python1.py
+++ labs3python/labs3python1.py
@@ -1,14 +1,16 @@
-import pprint
 import copy
+import pprint
 def rec(n, s):
-    if n == 1:
-        return [f"level {n}"] * s
-    r = []
-    for i in range(s):
-        r.append(rec(n-1,s))
-    return r
-print("Рекурсивный")
-pprint.pprint(rec(2, 3))
+    def b(d):
+        if d == 1:
+            return [f"level {n}"] * s
+        r = []
+        for i in range(s):
+            r.append(b(d-1))
+        return r
+    return b(n)
+
+
 
 def mas(n, s):
     if n <= 0:
@@ -21,5 +23,27 @@ def mas(n, s):
         a = r
     return a 
 
+def format_ndim(arr, level=0):
+    indent = '    ' * level
+    if isinstance(arr, list):
+        if not arr:
+            return indent + '[]'
+        if all(not isinstance(e, list) for e in arr):
+            return indent + '[' + ', '.join(repr(e) for e in arr) + ']'
+        else:
+            lines = [indent + '[']
+            for i, e in enumerate(arr):
+                lines.append(format_ndim(e, level + 1))
+                if i < len(arr) - 1:
+                    lines[-1] += ','
+            lines.append(indent + ']')
+            return '\n'.join(lines)
+    else:
+        return indent + repr(arr)
+
+print("Рекурсивный")
+print(format_ndim(rec(3, 2)))
+
+
 print("Нерекурсивный")
-pprint.pprint(mas(2,3))
+print(format_ndim(mas(3, 2)))
